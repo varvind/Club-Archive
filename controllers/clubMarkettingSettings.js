697de7784b7f63796a137548eccbdd9e9fd8877f
varvind <varvind@tamu.edu>
2020-05-24 18:54:35 -0500

    canEdit var missing

--- controllers/clubMarkettingSettings.js
+++ controllers/clubMarkettingSettings.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     const user = await User.findById(req.session.userId);
     const club = await Club.findById(req.params.id)
 
-
+    var canEdit = false
     if(user != null){
         for(var i = 0; i < user.clubs.length; i++){
             if(String(user.clubs[i]) == String(club._id)){
--- controllers/clubadminappsettings.js
+++ controllers/clubadminappsettings.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 module.exports = async (req, res) => {
     const user = await User.findById(req.session.userId)
     const club = await Club.findById(req.params.id)
-    
+    var canEdit = false
     if(user != null){
         for(var i = 0; i < user.clubs.length; i++){
             if(String(user.clubs[i]._id) == String(club._id)){
