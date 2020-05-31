c3f7fa54c18dc700a653637382bced4ef2a04812
varvind <varvind@tamu.edu>
2020-05-31 14:17:11 -0500

    fixed delete club bug, club deletes and removes the club from each admins club list

--- controllers/clubprofilesettings.js
+++ controllers/clubprofilesettings.js
@@ -1,9 +1,13 @@
 const User = require('../models/User')
 const Club = require('../models/Club')
+const CAApp = require('../models/CAApprovedApps')
 module.exports = async (req, res) => {
     const user = await User.findById(req.session.userId)
     const club = await Club.findById(req.params.id)
+
+    const applications =  await CAApp.find({})
     var canEdit = false;
+    var appSubmitted = false;
     if(user != null){
         for(var i = 0; i < user.clubs.length; i++){
             if(String(user.clubs[i]) == String(club._id)){
@@ -17,11 +21,18 @@ module.exports = async (req, res) => {
                 break;
             }
         }
+        for(var i = 0; i < applications.length;i++){
+            if(String(applications[i].club._id) == String(club._id)){
+                appSubmitted = true
+                break
+            }
+        }   
     }
     if (canEdit){
         res.render('clubProfileSettings' , {
             user,
-            club
+            club,
+            appSubmitted
         })
     }
     else{
--- controllers/confirmclubdelete.js
+++ controllers/confirmclubdelete.js
@@ -5,12 +5,23 @@ module.exports = async (req, res) => {
     const club = await Club.findById(req.params.id);
     const users = []
     
-    (await User.find({clubs : [club._id]})).forEach(function(user) {
-        console.log(user)
-    });
+    for(var i = 0; i < club.adminstrators.length; i++){
+        var user = await User.findById(club.adminstrators[i].id);
+        for(var j = 0; j < user.clubs.length; j++){
+            if(user.clubs[j] == String(club._id)){
+                user.clubs.splice(j,1)
+                user.save()
+            }
+        
+        }
+    }
 
     await Club.deleteOne({"_id" : club._id})
-
+    for(var i = 0; i < searches.length; i++){
+        if(String(searches[i]._id) == String(club._id)){
+            searches.splice(i,1)
+        }
+    }
     setTimeout(() => {
         res.redirect('/')
     }, 4000)
