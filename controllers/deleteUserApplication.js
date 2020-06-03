4f70f900c168d6f0fdedf048ae99e46797e6fef9
varvind <varvind@tamu.edu>
2020-06-03 07:39:24 -0500

    Small change to allow user to remove admin application from consideration if they choose

--- controllers/deleteUserApplication.js
+++ controllers/deleteUserApplication.js
@@ -1,14 +1,22 @@
 const User = require('../models/User')
-
+const Club = require('../models/Club')
 
 module.exports = async (req, res) => {
  
     const user = await User.findById(req.session.userId)
-
+    const club = await Club.findById(req.params.id)
     for(var i = 0; i < user.pending_applications.length; i++) {
         if(String(user.pending_applications[i].id) == String(req.params.id)) {
             user.pending_applications.splice(i,1)
             user.save()
+            break;
+        }
+    }
+    for(var i = 0; i < club.admin_applications.length;i++ ){
+        if(String(req.session.userId) == club.admin_applications[i].id){
+            club.admin_applications.splice(i,1)
+            club.save();
+            break;
         }
     }
 
