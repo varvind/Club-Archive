86113d04b56ed27231e12209c38b8b4a96f102ae
qromanoski <quentinromanoski@gmail.com>
2020-06-08 19:58:33 -0700

    No duplicate members

--- controllers/approveMemberApplication.js
+++ controllers/approveMemberApplication.js
@@ -29,18 +29,43 @@ module.exports = async (req, res) =>{
     
 
     if(req.body.approve){
-        member = {name : club.member_applications[i].fullname, id:req.params.user_id}
-        club.members.push(member)
+        member = {name : club.member_applications[i].fullname, id:user._id}
+        let insert1 = false
+        club.members.forEach(member => {
+            if(String(member.id) == String(user._id)){
+                //console.log("User already in clum-members schema")
+                insert1 = true
+            }
+        });
+        if(insert1 == false){
+            //console.log("User not found in club-member schema, so push user as new member")
+            club.members.push(member)
+            club.save()
+        }
+        //else{console.log("Already a Member (club)")}
         club.member_applications.splice(club_application_index,1)
         club.save()
-
        
         user.pending_applications[user_application_index].status = "Approved"
         user.markModified('pending_applications')
+        let insert2 = false
+        user.clubs.forEach(found => {
+            if(String(found) == String(club._id)){
+                //console.log("Club already in user-club schema")
+                insert2 = true
+            }
+        });
+        if(insert2 == false){
+            //console.log("Club not found in user-club schema, so push new club")
+            user.clubs.push(club._id)
+            user.save()
+        }
+        else{
+            //console.log("Already a Member (user)")
+            user.pending_applications.splice(user_application_index,1)
+            user.save()
+        }
         
-        let insert = user.clubs.includes(req.params.club_id)
-        if(!insert){user.clubs.push(req.params.club_id)}
-        user.save()
     }
     else{
         club.member_applications.splice(club_application_index,1)
