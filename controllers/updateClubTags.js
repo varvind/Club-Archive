e67cf01f1ddfbb1e8d23b83e86cf57a127ae7488
varvind <varvind@tamu.edu>
2020-06-10 20:22:20 -0500

    Club Tags fixed

--- controllers/updateClubTags.js
+++ controllers/updateClubTags.js
@@ -4,7 +4,6 @@ const Club = require('../models/Club')
 module.exports = async (req, res) => {
     const user = await User.findById(req.session.userId);
     const club = await Club.findById(req.params.id)
-
     if(req.body.tags != ""){
         if(!club.tags.includes(req.body.tags)){
             club.tags.push(req.body.tags)
