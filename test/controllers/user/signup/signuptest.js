47613e06ae305a02640b3e6dd6e815c78433dc24
varvind <varvind@tamu.edu>
2020-08-02 11:42:22 -0500

    made updates to test

--- test/controllers/user/signup/signuptest.js
+++ test/controllers/user/signup/signuptest.js
@@ -3,7 +3,6 @@ const request = require('supertest')
 const mongoose = require('mongoose')
 const app = require('../../../../index')
 describe('POST /addUser', function () {
-    this.timeout(10000)
     before((done) => {
         const Mockgoose = require('mockgoose').Mockgoose
         const mockgoose = new Mockgoose(mongoose)
@@ -11,28 +10,29 @@ describe('POST /addUser', function () {
             .then(()=> {
                 mongoose.Promise = global.Promise;
                 mongoose.connect("mongodb://localhost/club_archive")
+                done()
             })
         .then(() => done())
         .catch((err) => done(err))
     })
 
     it('Test Successful Sign Up', (done) => {
-        this.timeout(10000)
-        setTimeout(done, 300)
+
         request(app).post('/addUser').send({firstName: 'test', lastName:'user', email : 'test@email.com', userName: "0",  password:'0', confirm_password: '0', major: 'comp sci', gradYear:'2020', image:''})
         .then((res) => {
             expect(res.header.location).to.equal('/')
+            done()
         }).catch((err) => done(err))
-    }).timeout(10000)
+    })
 
     it('Test Unsuccessful Sign Up', (done) => {
-        this.timeout(10000)
-        setTimeout(done, 300)
+
         request(app).post('/addUser').send({firstName: '', lastName:'user', email : 'test@email.com', userName: "0",  password:'0', confirm_password: '0', major: 'comp sci', gradYear:'2020', image:''})
         .then((res) => {
             expect(res.header.location).to.not.equal('/')
+            done()
         }).catch((err) => done(err))
-    }).timeout(10000)
+    })
 
 
     
