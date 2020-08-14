47b3e435b5dbc64bb8984f1e2c3157c7dfa3f6eb
varvind <varvind@tamu.edu>
2020-08-13 19:12:28 -0500

    updated travis tests

--- test/controllers/user/signup/impl/signuptest.js
+++ test/controllers/user/signup/impl/signuptest.js
@@ -4,18 +4,14 @@ const mongoose = require('mongoose')
 const app = require('../../../../../index')
 const Mockgoose = require('mockgoose').Mockgoose
 const mockgoose = new Mockgoose(mongoose)
+const User = require('../../../../../models/User')
 describe('POST /addUser', function () {
-    this.timeout(120000)
-    // before((done) => {
-    //     this.timeout(120000)
-    //     mockgoose.prepareStorage()
-    //         .then(()=> {
-    //             mongoose.Promise = global.Promise;
-    //             mongoose.connect("mongodb://localhost/club_archive")
-    //         })
-    //     .then(() => done())
-    //     .catch((err) => done(err))
-    // })
+
+    before((done) => {
+        User.deleteOne({userName: "0"}, function(err, obj) {
+            done()
+        })
+    })
 
     it('Test Successful Sign Up', (done) => {
 
@@ -26,29 +22,22 @@ describe('POST /addUser', function () {
         }).catch((err) => done(err))
     })
 
-    // it('Test Unsuccessful Sign Up', (done) => {
+    it('Test Unsuccessful Sign Up', (done) => {
 
-    //     request(app).post('/addUser').send({firstName: '', lastName:'user', email : 'test@email.com', userName: "0",  password:'0', confirm_password: '0', major: 'comp sci', gradYear:'2020', image:''})
-    //     .then((res) => {
-    //         expect(res.header.location).to.not.equal('/')
-    //         done()
-    //     }).catch((err) => done(err))
-    // })
-
-    it('Test Confirm Password does not match Password', (done) => {
-        request(app).post('/addUser').send({firstName: '', lastName:'user', email : 'test@email.com', userName: "0",  password:'2', confirm_password: '0', major: 'comp sci', gradYear:'2020', image:''})
+        request(app).post('/addUser').send({firstName: '', lastName:'user', email : 'test@email.com', userName: "0",  password:'0', confirm_password: '0', major: 'comp sci', gradYear:'2020', image:''})
         .then((res) => {
             expect(res.header.location).to.not.equal('/')
             done()
         }).catch((err) => done(err))
     })
-    
-    // after((done) => {
-    //     mockgoose.helper.reset().then(() => {
-    //         done()
-    //     });
-    //     this.timeout(120000)
-    // })
+
+    it('Test Confirm Password does not match Password', (done) => {
+        request(app).post('/addUser').send({firstName: 'test', lastName:'user', email : 'test@email.com', userName: "0",  password:'2', confirm_password: '0', major: 'comp sci', gradYear:'2020', image:''})
+        .then((res) => {
+            expect(res.header.location).to.not.equal('/')
+            done()
+        }).catch((err) => done(err))
+    })    
     
 
 })
