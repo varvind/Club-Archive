b0693467e48cb01a2998aaa899123888f1409720
varvind <varvind@tamu.edu>
2020-08-17 12:53:05 -0500

    [CAB-92] Test User Profile View - Added tests and fixed checkstyle errors for userprofile controller

--- controllers/user/profile/view/userprofile.js
+++ controllers/user/profile/view/userprofile.js
@@ -1,38 +1,59 @@
 const User = require('../../../../models/User')
 const Club = require('../../../../models/Club')
-
-
+var mode = 'production'
+var loggedIn = global.loggedIn
+var userId = ''
 module.exports = async (req, res) => {
-    if(!loggedIn){
-        res.redirect('/')
-    } else {
-        
-    const user = await User.findById(req.session.userId, (err, foundUser) => {
-        if(err || !foundUser){
-            console.log("Problem finding User")
-        }
+  await initVariables(mode, req.session.userId)
+  if (!loggedIn) {
+    res.redirect('/')
+  } else {
+    const user = await User.findById(userId, (err, foundUser) => {
+      if (err || !foundUser) {
+        console.log('Problem finding User')
+      }
     })
     const clubs = user.recent_search
-    const clubNames = new Array()
+    const clubNames = []
     user.clubs.forEach(eachClub => {
-        Club.findById(eachClub, (err, foundClub) => {
-            if(err || !foundClub){
-                console.log("No club found")
-            }else{
-                //console.log(foundClub.name)
-                clubNames.push(foundClub.name)
-            }
-        })
+      Club.findById(eachClub, (err, foundClub) => {
+        if (err || !foundClub) {
+          console.log('No club found')
+        } else {
+          clubNames.push(foundClub.name)
+        }
+      })
     })
 
-    setTimeout( () => {
-        res.render('user_views/profile/userProfile', {
-            user,
-            clubs,
-            clubNames,
-            layout : 'layouts/topMenuBar'
-        })
+    setTimeout(() => {
+      res.render('user_views/profile/userProfile', {
+        user,
+        clubs,
+        clubNames,
+        layout: 'layouts/topMenuBar'
+      })
     }, 2000)
-    }
-    
+  }
 }
+
+// These two methods are used for tests
+function testMode () {
+  mode = 'test'
+}
+
+async function initVariables (mode, id) {
+  if (mode === 'test') {
+    await User.findOne({ userName: '0' }, (err, user) => {
+      if (err) {
+        console.log(err)
+      }
+      userId = user._id
+    })
+    loggedIn = 'mock'
+  } else {
+    userId = id
+    loggedIn = global.loggedIn
+  }
+}
+
+module.exports.testMode = testMode
