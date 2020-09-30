51da9e8604c019141ea0cab1145ea633e0259760
varvind <varvind@tamu.edu>
2020-09-30 07:54:01 -0500

    [CAB-94] Added Tests for updateUser.js

--- controllers/user/settings/impl/updateUser.js
+++ controllers/user/settings/impl/updateUser.js
@@ -1,9 +1,14 @@
 const User = require('../../../../models/User')
 const mongoose = require('mongoose')
-
-module.exports = (req, res)=> {
+var mode = 'production'
+var loggedIn = global.loggedIn
+var userId = '';
+module.exports = async (req, res)=> {
+  await initVariables(mode, req.session.userId)
+  if(!loggedIn) {
+    res.redirect('/')
+  } else {
     var gfs = global.gfs
-    var userid = req.session.userId;
     var first = req.body.firstName;
     var last = req.body.lastName;
     var email = req.body.email;
@@ -12,49 +17,71 @@ module.exports = (req, res)=> {
     var grad = req.body.gradYear;
     var minor = req.body.minor;
     if(!req.file){
-        User.findById(userid, (error, user)=>{
-            if(first != ""){ user.firstName = first; }
-            if(last != ""){ user.lastName = last; }
-            if(email != ""){ user.email = email; }
-            if(userName != ""){ user.userName = userName }
-            if(major!= ""){ user.major = major; }
-            if(grad != ""){ user.gradYear = grad; }
-            if(minor !=""){user.minor = minor}
-            user.save();
-        });
-        res.redirect('/usersettings')
+      await User.findById(userId, (error, user)=>{
+        if(first != ""){ user.firstName = first; }
+        if(last != ""){ user.lastName = last; }
+        if(email != ""){ user.email = email; }
+        if(userName != ""){ user.userName = userName }
+        if(major!= ""){ user.major = major; }
+        if(grad != ""){ user.gradYear = grad; }
+        if(minor !=""){user.minor = minor}
+        user.save();
+      });
+      res.redirect('/usersettings')
     }
     else{  
-        User.findById(userid, (error, user)=>{
-            if(first != ""){ user.firstName = first; }
-            if(last != ""){ user.lastName = last; }
-            if(email != ""){ user.email = email; }
-            if(userName != ""){ user.userName = userName }
-            if(major!= ""){ user.major = major; }
-            if(grad != ""){ user.gradYear = grad; }
-            if(minor !=""){user.minor = minor}
-            
-            //delete previous image
-            if(user.image.filename != "default") {
-                gfs.remove({_id: new mongoose.Types.ObjectId(user.image.id), root: 'uploads'}, (err) => {
-                    if(err){
-                        console.log("Issue with deleting old user image files")
-                        res.redirect('/')
-                    }else{
-                        console.log("Successfully Deleted Old image files")
-                    }
-                })
-            }
-    
-            user.image= {
-                filename: req.file.filename,
-                id: req.file.id
-            }
+      await User.findById(userId, (error, user)=>{
+        if(first != ""){ user.firstName = first; }
+        if(last != ""){ user.lastName = last; }
+        if(email != ""){ user.email = email; }
+        if(userName != ""){ user.userName = userName }
+        if(major!= ""){ user.major = major; }
+        if(grad != ""){ user.gradYear = grad; }
+        if(minor !=""){user.minor = minor}
+        
+        //delete previous image
+        if(user.image.filename != "default") {
+            gfs.remove({_id: new mongoose.Types.ObjectId(user.image.id), root: 'uploads'}, (err) => {
+                if(err){
+                    console.log("Issue with deleting old user image files")
+                    res.redirect('/')
+                }else{
+                    console.log("Successfully Deleted Old image files")
+                }
+            })
+        }
 
-            user.save();
-            console.log(error)
-        });
-        res.redirect('/usersettings')
+        user.image= {
+            filename: req.file.filename,
+            id: req.file.id
+        }
+
+        user.save();
+        console.log(error)
+      });
+      res.redirect('/usersettings')
     }
     
+  }
+    
 }
+
+function testMode () {
+  mode = 'test'
+}
+async function initVariables (mode, id) {
+  if (mode === 'test') {
+    await User.findOne({ userName: '0' }, (err, user) => {
+      if (err) {
+        console.log(err)
+      }
+      userId = user._id
+    })
+    loggedIn = 'mock'
+  } else {
+    userId = id
+    loggedIn = global.loggedIn
+  }
+}
+
+module.exports.testMode = testMode
